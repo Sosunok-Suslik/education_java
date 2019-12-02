73e524832df3740a75f22e3d1675ad6ef8a6ed52
Suslik Sosinok <nv781467@gmail.com>
2019-12-02 17:02:34 +0300

    complete task1-3

--- src/main/java/lesson01/part1/Task02.java
+++ src/main/java/lesson01/part1/Task02.java
@@ -19,7 +19,8 @@ public class Task02 {
      * 5. Попробуй удалить метод main, в пустом месте написать psvm и нажать tab.
      */
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!\nHello World!\nHello World!");
-    }
+           public static void main (String[] args) {
+            System.out.println("Hello World!\nHello World!\nHello World!");
+           }
 }
+
